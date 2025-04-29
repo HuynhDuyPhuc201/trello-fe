1ec9d54920eed8011654cfb02dfcc6ad5b119e48
unknown <hdphuc201@gmail.com>
2025-04-29 09:06:28 +0700

    fix: change url API

--- src/services/card.service.js
+++ src/services/card.service.js
@@ -1,4 +1,4 @@
-import api from '~/config/api'
+import api from '~/apis'
 
 export const cardService = {
   async createNewCard(newCardData) {
--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,4 +1,4 @@
-import api from '~/config/api'
+import api from '~/apis'
 
 export const columnService = {
   async createNewColumn(newColumnData) {
